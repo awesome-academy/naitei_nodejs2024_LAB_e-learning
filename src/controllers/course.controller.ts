awesome-academy/LAB_e-learning

Implement User/Course CRUD

src/controllers/course.controller.ts
@@ -1,0 +1,75 @@
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import { getAllCourses, 
+  getCourseById, 
+  createCourse, 
+  deleteCourse, 
+  updateCourse, 
+} from '../services/course.service';
+
+export const courseList = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courses = await getAllCourses();
+      res.status(200).json(courses);
+    });
+
+export const courseDetails = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseId = parseInt(req.params.id);
+      const course = await getCourseById(courseId);
+
+      if (course) {
+        res.status(200).json(course);
+      } else {
+        res.status(404).json({ message: 'Course not found' });
+      }
+    });
+
+export const createCourseGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Provide course data to create a course' });
+    });
+
+export const createCoursePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseData = req.body; 
+      const newCourse = await createCourse(courseData);
+      res.status(201).json(newCourse);
+    });
+
+export const deleteCourseGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Confirm course deletion' });
+    });
+
+export const deleteCoursePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseId = parseInt(req.params.id);
+      const course = await getCourseById(courseId);
+
+      if (course) {
+        await deleteCourse(course);
+        res.status(200).json({ message: 'Course deleted' });
+      } else {
+        res.status(404).json({ message: 'Course not found' });
+      }
+    });
+
+export const updateCourseGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Provide updated course data' });
+    });
+
+export const updateCoursePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseId = parseInt(req.params.id);
+      const course = await getCourseById(courseId);
+
+      if (course) {
+        const updatedData = req.body;
+        const updatedCourse = await updateCourse(course, updatedData);
+        res.status(200).json(updatedCourse);
+      } else {
+        res.status(404).json({ message: 'Course not found' });
+      }
+    });

src/controllers/user.controller.ts
@@ -1,0 +1,85 @@
+import { getAllUsers, 
+    getUserById, 
+    createUser, 
+    updateUser, 
+    deleteUser 
+} from '../services/user.service';
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+
+export const userList = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const users = await getAllUsers();
+        res.status(200).json(users); 
+});
+
+export const userDetails = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        const user = await getUserById(userId);
+            
+        if (user) {
+            res.status(200).json(user); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});
+
+export const userCreateGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+        res.status(200).json({ message: 'Display user creation form' }); 
+});
+
+export const userCreatePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const newUserData = req.body; 
+        const newUser = await createUser(newUserData);
+        res.status(201).json(newUser); 
+});
+
+export const userDeleteGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        const user = await getUserById(userId);
+            
+        if (user) {
+            res.status(200).json({ message: 'Confirm deletion of user', user }); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});
+
+export const userDeletePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        await deleteUser(userId); 
+        res.status(204).send(); 
+});
+
+export const userUpdateGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        const user = await getUserById(userId);
+            
+        if (user) {
+            res.status(200).json(user); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});
+
+export const userUpdatePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userToUpdate = await getUserById(parseInt(req.params.id));
+        const updatedUserData = req.body; 
+        if (!userToUpdate) {
+            throw new Error('User not found');
+        }
+        const updatedUser = await updateUser(userToUpdate, updatedUserData);
+            
+        if (updatedUser) {
+            res.status(200).json(updatedUser); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});

awesome-academy/naitei_nodejs2024_LAB_e-learning

src/controllers/course.controller.ts
@@ -1,0 +1,75 @@
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import { getAllCourses, 
+  getCourseById, 
+  createCourse, 
+  deleteCourse, 
+  updateCourse, 
+} from '../service/course.service';
+
+export const courseList = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courses = await getAllCourses();
+      res.status(200).json(courses);
+    });
+
+export const courseDetails = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseId = parseInt(req.params.id);
+      const course = await getCourseById(courseId);
+
+      if (course) {
+        res.status(200).json(course);
+      } else {
+        res.status(404).json({ message: 'Course not found' });
+      }
+    });
+
+export const createCourseGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Provide course data to create a course' });
+    });
+
+export const createCoursePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseData = req.body; 
+      const newCourse = await createCourse(courseData);
+      res.status(201).json(newCourse);
+    });
+
+export const deleteCourseGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Confirm course deletion' });
+    });
+
+export const deleteCoursePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseId = parseInt(req.params.id);
+      const course = await getCourseById(courseId);
+
+      if (course) {
+        await deleteCourse(course);
+        res.status(200).json({ message: 'Course deleted' });
+      } else {
+        res.status(404).json({ message: 'Course not found' });
+      }
+    });
+
+export const updateCourseGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Provide updated course data' });
+    });
+
+export const updateCoursePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+      const courseId = parseInt(req.params.id);
+      const course = await getCourseById(courseId);
+
+      if (course) {
+        const updatedData = req.body;
+        const updatedCourse = await updateCourse(course, updatedData);
+        res.status(200).json(updatedCourse);
+      } else {
+        res.status(404).json({ message: 'Course not found' });
+      }
+    });

src/controllers/user.controller.ts
@@ -1,0 +1,85 @@
+import { getAllUsers, 
+    getUserById, 
+    createUser, 
+    updateUser, 
+    deleteUser 
+} from '../service/user.service';
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+
+export const userList = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const users = await getAllUsers();
+        res.status(200).json(users); 
+});
+
+export const userDetails = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        const user = await getUserById(userId);
+            
+        if (user) {
+            res.status(200).json(user); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});
+
+export const userCreateGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+        res.status(200).json({ message: 'Display user creation form' }); 
+});
+
+export const userCreatePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const newUserData = req.body; 
+        const newUser = await createUser(newUserData);
+        res.status(201).json(newUser); 
+});
+
+export const userDeleteGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        const user = await getUserById(userId);
+            
+        if (user) {
+            res.status(200).json({ message: 'Confirm deletion of user', user }); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});
+
+export const userDeletePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        await deleteUser(userId); 
+        res.status(204).send(); 
+});
+
+export const userUpdateGet = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userId = parseInt(req.params.id); 
+        const user = await getUserById(userId);
+            
+        if (user) {
+            res.status(200).json(user); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});
+
+export const userUpdatePost = 
+    asyncHandler(async (req: Request, res: Response) => {
+        const userToUpdate = await getUserById(parseInt(req.params.id));
+        const updatedUserData = req.body; 
+        if (!userToUpdate) {
+            throw new Error('User not found');
+        }
+        const updatedUser = await updateUser(userToUpdate, updatedUserData);
+            
+        if (updatedUser) {
+            res.status(200).json(updatedUser); 
+        } else {
+            res.status(404).json({ message: 'User not found' }); 
+        }
+});