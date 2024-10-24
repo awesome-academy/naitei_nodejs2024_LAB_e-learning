41479d6ebf7276e260a3bc1786e0d6a472876ef3
thanhbao922003 <thanhbao2003@gmail.com>
2024-10-24 22:45:01 +0700

    Professor Panel Fix

---
+++ src/controller/professor/professorLesson.controller.ts
@@ -0,0 +1,109 @@
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import { getLessonsBySectionIds, createLesson, updateLesson, deleteLesson } from 'src/service/lession.service';
+import { getCoursesByUserId  } from 'src/service/course.service';
+import { getSectionsByCourseIds } from 'src/service/section.service';
+
+export const professorLesonShowGet = asyncHandler(async (req: Request, res: Response) => {
+    try {
+        const userId = req.session!.user?.id;
+        const isLoggedIn = Boolean(userId);
+        
+        if (!isLoggedIn) {
+        return res.status(403).render('error', { message: req.t('admin.not_logged_in') }); 
+        }
+        
+        const courses = await getCoursesByUserId(userId); 
+        const courseIds = courses.map(course => course.id);
+        const sections = await getSectionsByCourseIds(courseIds);
+        const sectionId = sections.map(section => section.id);
+        const lessons =  await getLessonsBySectionIds(sectionId);
+
+        if (!lessons) {
+        return res.status(404).render('error', { message: req.t('admin.user_not_found') }); 
+        }
+
+        return res.render('professor/lessonManagement', {
+        title: req.t('admin.user_management_title'),
+        message: req.t('admin.user_management_message'),
+        lessons, 
+        sections,
+        isLoggedIn,
+        t: req.t,
+        });
+    } catch (error) {
+        console.error(error); 
+        res.status(500).render('error', { message: req.t('course.course_error') });
+    }
+});
+
+export const professorCreateLesson = async (req: Request, res: Response) => {
+    try {
+        const sectionIds = req.body.section_id;
+        const names = req.body.name;
+        const types = req.body.type;
+        const contents = req.body.content;
+        const descriptions = req.body.description;
+        const times = req.body.time;
+
+        for (let i = 0; i < names.length; i++) {
+            const sectionId = Number(sectionIds[i]);
+      
+            if (isNaN(sectionId)) {
+                throw new Error("Invalid sectionId");
+            }
+      
+            await createLesson({
+                name: names[i],
+                type: types[i],
+                section_id: sectionId, 
+                content: contents[i],
+                description: descriptions[i],
+                time: times[i],
+            });
+          }
+
+        res.redirect(`/professors/lessons`);
+    } catch (error) {
+        res.status(400).render('error', { message: error.message });
+    }
+};
+
+export const professorUpdateLesson = async (req: Request, res: Response) => {
+    try {
+        const lessonId = Number(req.body.id);
+        const sectionId = Number(req.body.section_id); 
+        if (isNaN(lessonId) || isNaN(sectionId)) {
+            res.status(400).render('error', { message: 'Invalid lesson ID or section ID.' });
+            return;
+        }
+
+        const updatedLesson = await updateLesson(lessonId, req.body);
+        
+        if (!updatedLesson) {
+            res.status(404).render('error', { message: 'Lesson not found.' });
+            return;
+        }
+
+        res.redirect(`/professors/lessons`);
+    } catch (error) {
+        res.status(400).render('error', { error: error.message || 'An unexpected error occurred.' });
+      }
+};
+
+
+export const professorDeleteLesson = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const success = await deleteLesson(Number(id)); 
+
+        if (!success) {
+        res.status(404).render('error', { message: 'Lesson not found.' });
+        return;
+        }
+        res.status(204).send(); 
+        // res.redirect(`/professors/lessons`);
+    } catch (error) {
+        res.status(400).render('error', { message: error.message });
+    }
+};
--- src/service/course.service.ts
+++ src/service/course.service.ts
@@ -28,21 +28,13 @@ export async function getAllCourses() {
   });
 }
 
-export const getPaymentsByUserId = async (
-  userId: number
-): Promise<Payment[]> => {
-  const paymentRepository = AppDataSource.getRepository(Payment);
-
+export const getPaymentsByUserId = async (userId: number): Promise<Payment[]> => {  
   return await paymentRepository.find({
     where: { id: userId },
   });
 };
 
-export const getCoursesByIds = async (
-  courseIds: number[]
-): Promise<Course[]> => {
-  const courseRepository = AppDataSource.getRepository(Course);
-
+export const getCoursesByIds = async (courseIds: number[]): Promise<Course[]> => {
   return await courseRepository.findBy({
     id: In(courseIds),
   });
@@ -66,8 +58,54 @@ export async function getProfessorByCourse(courseId: number) {
     where: { id: courseId },
     relations: { professor: true },
   });
-
   return course?.professor;
+}
+
+export const getCoursesByUserId = async (userId: number) => {
+  return await courseRepository.find({ where: { professor_id: userId } });
+};
+
+export async function getCoursesInfo(professorId: number): Promise<any[]> {
+  const professor = await userRepository.findOne({
+    where: { id: professorId, role: 'professor' },
+  });
+
+  if (!professor) {
+    throw new Error(`Professor with ID ${professorId} not found.`);
+  }
+
+  const courses = await courseRepository.find({
+    where: { professor: { id: professorId } },
+    relations: ['category', 'professor'],
+  });
+
+  const coursesWithEnrollmentAndPaymentCount = await Promise.all(
+    courses.map(async (course) => {
+      const enrollmentCount = await enrollmentRepository.count({
+        where: { course: { id: course.id } },
+      });
+
+      const paymentCount = await paymentRepository.count({
+        where: { course: { id: course.id } },
+      });
+
+      return {
+        ...course,
+        enrollmentCount: enrollmentCount,
+        paymentCount: paymentCount,
+      };
+    })
+  );
+
+  return coursesWithEnrollmentAndPaymentCount;
+}
+
+export async function getCategoryByCourse(courseId: number) {
+  const course = await courseRepository.findOne({
+    where: { id: courseId },
+    relations: { category: true },
+  });
+  return course?.category;
 }
 
 export const getCoursesWithSectionsAndHours = async () => {
@@ -85,8 +123,9 @@ export const getCoursesWithSectionsAndHours = async () => {
       );
 
       const professor = await getProfessorByCourse(course.id);
-      const professorName = professor?.name || "Unknown";
-      const professorId = professor?.id || "Unknown";
+
+      const professorName = professor?.name || 'Unknown'; 
+      const professorId = professor?.id || 'Unknown';
 
       return {
         ...course,
@@ -146,40 +185,45 @@ export async function countEnrolledUsersInCourse(
 
 export async function createCourse(data: Partial<Course>): Promise<Course> {
   const newCourse = courseRepository.create({
-    name: data.name,
-    description: data.description,
-    price: data.price,
-    average_rating: data.average_rating,
-    professor_id: data.professor_id,
+      name: data.name,
+      description: data.description,
+      price: data.price, 
+      category_id: data.category_id,
+      average_rating: data.average_rating, 
+      professor_id: data.professor_id 
   });
 
   return await courseRepository.save(newCourse);
 }
 
-export const updateCourse = async (id: number, courseData: any) => {
+export const checkProfessorAuthorization = async (courseId: number, userId: number) => {
   const course = await courseRepository.findOne({
-    where: { id },
-    relations: ["professor", "sections"],
+    where: { id: courseId },
+    relations: ['professor'],
   });
+
+  if (!course) {
+    return false;
+  }
+
+  return course.professor.id === userId; 
+};
+
+export const updateCourse = async (id: number, courseData: any) => {
+  const course = await courseRepository.findOne({ where: { id }, relations: ['professor', 'sections'] });
 
   if (!course) {
     throw new Error(`Course with ID ${id} not found.`);
   }
 
   course.name = courseData.name;
-
-  const professor = await getProfessorByCourse(courseData.professor_id);
-  if (professor) {
-    course.professor = professor;
-    course.professor_id = professor.id;
-  }
-
-  course.description = courseData.description;
-  course.price = courseData.price;
-  course.average_rating = courseData.average_rating;
+  course.category_id = courseData.category_id;
+  course.description = courseData.description; 
+  course.price = courseData.price; 
+  course.average_rating = courseData.average_rating; 
 
   return await courseRepository.save(course);
-};
+}
 
 export async function deleteCourse(id: number): Promise<boolean> {
   const result = await courseRepository.delete(id);
@@ -210,7 +254,6 @@ export const filterAndSortCourses = async (
     .createQueryBuilder("course")
     .innerJoinAndSelect("course.category", "category");
 
-  // Apply filters
   if (filters.professorId) {
     query.andWhere("course.professor_id = :professorId", {
       professorId: filters.professorId,
